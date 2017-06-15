1b1556342a8e8d56040409ce95e4791a512f1803
lindseygilmore <lindseygilmore21@gmail.com>
2017-06-15 14:45:46 -0500

    spacing fixed

--- js/site.js
+++ js/site.js
@@ -4,3 +4,10 @@ $(".js-hamburger").on("click", function(event){
 	event.preventDefault();
 	$("nav li").toggle();
 });
+
+
+var button = document.getElementById('linkedinButton')
+
+	button.addEventListener('click', function(){
+		window.open('https://www.linkedin.com/in/lindsey-gilmore-037b8031/')
+}
